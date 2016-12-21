d033cb24dd890c1474ce7a642147176dd9171048
DimitriRV <Dimitri_s@web.de>
2016-12-21 16:13:01 +0100

    new fields to display time of current session. define fields in controller

--- src/main/session/Controller.java
+++ src/main/session/Controller.java
@@ -1,5 +1,23 @@
 package main.session;
 
-public class Controller {
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
 
+public class Controller {
+	
+	private Model session;
+	
+	private ComboBox projectsCombo;
+	
+	private ComboBox tasksCombo;
+	
+	private TextField bemerkungField;
+	
+	private TextField pauseField;
+	
+	private TextField beginnField;
+	
+	private TextField endeField;
+	
+	
 }
