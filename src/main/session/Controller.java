71179ed4f6b5e01116f508165ea4e3c21076cb8c
DimitriRV <Dimitri_s@web.de>
2016-12-23 17:22:44 +0100

    change some layout styles new model for comboboxes with id value pares init Dao Project Classe with initial values, without db

--- src/main/session/Controller.java
+++ src/main/session/Controller.java
@@ -1,23 +1,42 @@
 package main.session;
 
+import base.dao.Projects;
+import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import base.model.IntegerStringModel;
 
 public class Controller {
 	
 	private Model session;
 	
-	private ComboBox projectsCombo;
+	@FXML
+	private ComboBox<IntegerStringModel> projectsCombo;
 	
-	private ComboBox tasksCombo;
+	@FXML
+	private ComboBox<IntegerStringModel> tasksCombo;
 	
-	private TextField bemerkungField;
+	@FXML
+	private TextArea bemerkungField;
 	
+	@FXML
 	private TextField pauseField;
 	
+	@FXML
 	private TextField beginnField;
 	
+	@FXML
 	private TextField endeField;
 	
+	private Projects projectsDao; 
 	
+	
+	@FXML
+	private void initialize() {
+		
+		projectsDao = new Projects();
+		projectsCombo.setItems(projectsDao.getEnumData());
+		
+	}
 }
