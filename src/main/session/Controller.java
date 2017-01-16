9574a60a3aa134638229efe87afe247096f444b9
DimitriRV <Dimitri_s@web.de>
2017-01-16 17:00:54 +0100

    Model params includet, setters +  getters View Start Stop Buttons in ToggleButtons converted Controller: ToggleGroup for start stop buttons created, listener addet to handle stop start. create session model function created save session function created

--- src/main/session/Controller.java
+++ src/main/session/Controller.java
@@ -1,17 +1,29 @@
 package main.session;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 import base.enumeration.Projects;
 import base.enumeration.Tickets;
 import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.Toggle;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.control.ToggleGroup;
 
 public class Controller {
+	
+	
+	private ToggleGroup startStopGroup;
+	private Model session;
+	
 	
 	@FXML
 	private ComboBox<base.enumeration.Projects.Model> projectCombo;
 	
 	@FXML
 	private ComboBox<base.enumeration.Tickets.Model> ticketCombo;
+	
 	
 //	@FXML
 //	private TextArea commitMessage; 
@@ -28,11 +40,33 @@ public class Controller {
 		ticketCombo.getItems().setAll(Tickets.getInstance().getData(null));
 		projectCombo.valueProperty().addListener((observable, oldV, newV) -> handleProjectSelection(newV));
 		
+		startStopGroup = new ToggleGroup();
+		startStopGroup.selectedToggleProperty().addListener((observable,oldToggle,newToggle) -> handleStartStop(newToggle));
+		
 	}
+	
+	
+	public void handleStartStop(Toggle newToggle)
+	{
+		createNewSession();
+	}
+	
+	private void createNewSession()
+	{
+		session = new Model(projectCombo.getSelectionModel().getSelectedItem().getId(), ticketCombo.getSelectionModel().getSelectedItem().getId(), "");
+		session.setStartDate(LocalDateTime.now());
+	}
+	
+	private void saveSession()
+	{
+//		session.setBemerkung(bemerkung);
+		session.setEndDate(LocalDateTime.now());
+	}
+	
 	
 	public void handleProjectSelection(base.enumeration.Projects.Model newV)
 	{
-		ticketCombo.getItems().remove(0, ticketCombo.getItems().size());
+//		ticketCombo.getItems().remove(0, ticketCombo.getItems().size());
 		
 		if(newV != null)
 		{
--- src/main/session/Model.java
+++ src/main/session/Model.java
@@ -1,5 +1,91 @@
 package main.session;
 
-public class Model {
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+public class Model {
+	private IntegerProperty projectId;
+	private IntegerProperty ticketId;
+	private StringProperty bemerkung;
+	private ObjectProperty<LocalDateTime> startDate;
+	private ObjectProperty<LocalDateTime> endDate;
+	
+	/**
+	 * @param projectId
+	 * @param ticketId
+	 * @param bemerkung
+	 */
+	public Model(int projectId, int ticketId, String bemerkung) {
+		super();
+		this.projectId = new SimpleIntegerProperty(projectId);
+		this.ticketId = new SimpleIntegerProperty(ticketId);
+		this.bemerkung = new SimpleStringProperty(bemerkung);
+		this.startDate = new SimpleObjectProperty<LocalDateTime>();
+		this.endDate = new SimpleObjectProperty<LocalDateTime>();
+	}
+	
+	//Property getters
+	public IntegerProperty projectIdProperty() {
+		return projectId;
+	}
+	
+	public IntegerProperty ticketIdProperty() {
+		return ticketId;
+	}
+	
+	public StringProperty bemerkungProperty() {
+		return bemerkung;
+	}
+	
+	public ObjectProperty<LocalDateTime> startDateProperty() {
+		return startDate;
+	}
+	
+	public ObjectProperty<LocalDateTime> endDateProperty() {
+		return endDate;
+	}
+	
+	//getters
+	
+	public int getProjectId() 
+	{
+		return projectId.get();
+	}
+	public int getTicketId() {
+		return ticketId.get();
+	}
+	public String getBemerkung() {
+		return bemerkung.get();
+	}
+	public LocalDateTime getStartDate() {
+		return startDate.get();
+	}
+	public LocalDateTime getEndDate() {
+		return endDate.get();
+	}
+	
+	// setters
+	public void setEndDate(LocalDateTime endDate) {
+		this.endDate.set(endDate);
+	}
+	public void setProjectId(int projectId) {
+		this.projectId.set(projectId);
+	}
+	public void setTicketId(int ticketId) {
+		this.ticketId.set(ticketId);
+	}
+	public void setBemerkung(String bemerkung) {
+		this.bemerkung.set(bemerkung);
+	}
+	
+	public void setStartDate(LocalDateTime startDate) {
+		this.startDate.set(startDate);
+	}
 }
